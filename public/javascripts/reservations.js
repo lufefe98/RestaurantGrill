43caec96147b6e746ae2ddb69d7c213619a87ec4
lufefe98 <92111051+lufefe98@users.noreply.github.com>
2022-03-24 13:29:32 +0200

    Update reservations.js
    Added code to show that a table has been selected once it has been clicked.

--- public/javascripts/reservations.js
+++ public/javascripts/reservations.js
@@ -1,38 +1,7 @@
-// querySelectors 
-const container = document.querySelector('.container');
-const table = document.querySelectorAll('.row .seat:not(.occupied)');
+// SELECTING A SEAT
 
-const sbRadio = document.getElementById('sb-radio');
-const umhlangaRadio = document.getElementById('umhlanga-radio');
-const calendar = document.getElementById('calendar');
-const peopleCount = document.getElementById('dropdownMenuButton1');
-
-populateUI();
-
-
-
-
-
-// Save Seat Choice
-function setTableChoice(tableIndex, reservationDate) {
-    localStorage.setItem('selectedTable', tableIndex);
-    localStorage.setItem('selectedDate', reservationDate);
-  }
-
-
-
-
-
-// Update Seat Choice
-function updateTableChoice() {
-    const selectedTable = document.querySelectorAll('.row .table.selected');
-  
-    const tableIndex = [...selectedTable].map(seat => [...table].indexOf(table));
-  
-    localStorage.setItem('selectedTable', JSON.stringify(tableIndex));
-  
-    // const selectedTable = selectedSeats.length;
-
-    
-    setMovieData(movieSelect.selectedIndex, movieSelect.value);
-  }
+document.querySelectorAll('.table').forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('tableSelected')
+    })
+  })
