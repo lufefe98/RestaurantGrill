d82b05bdb68c5fdb7b4877c6c1b019460898b31e
lufefe98 <92111051+lufefe98@users.noreply.github.com>
2022-05-10 02:38:29 +0200

    Show more button functionality
    Settled on the code for attempt #1.

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -1,72 +1,36 @@
-// Attempt 1
+// Show more reviews after the first 5 reviews
 
 const moreBtn = document.querySelector('#showmore');
 
 let currentIndex = 5;
+let countOfReviewsToShow = 5;
 
-// moreBtn.addEventListener('click', () => {
-//   const reviews = [...document.querySelectorAll('.customerReviews')];
+moreBtn.addEventListener('click', () => {
+  const reviews = document.querySelectorAll('.customerReviews');
+  const hiddenReviews = [];
 
-//   for (let i = currentIndex; i <= reviews.length; i += 5) {
-//     if (reviews) {
-//       reviews.classList.add('showReview');      
-//     }
-//   }
+//   Create a list of hiddenReviews
+  for(let i = 0; i < reviews.length; i++) {
+    if(reviews[i].style.display === 'none') {
+      hiddenReviews.push(reviews[i]);
+    }
+  }
 
+//   Toggle the display of 5 hidden reviews
+  for (let i = 0; i < Math.min(hiddenReviews.length, countOfReviewsToShow); i++) {
+      hiddenReviews[i].style.display = 'block';
+  }
 
-//   // currentIndex += 5;
+  currentIndex += 5;
   
-//   // If you display all the elements then hide the show more button
-//   if (currentIndex >= reviews.length) {
-//     moreBtn.style.display = 'none';
-//   }
-// })
+//   If you display all the elements then hide the show more button
+  if (currentIndex >= reviews.length) {
+    moreBtn.style.display = 'none';
+  }
+})
 
 
-
-// ATTEMPT #2
-
-// create a function to implement the feature
-// function showMoreLess() {
-
-//   const moreBtn = document.querySelector('#showmore');
-//   const reviews = document.querySelectorAll('.customerReviews:nth-of-type(n+6)');
-
-//   if(reviews.style.display === 'none') {
-//     reviews.style.display = "block";
-//   }
-// }
-
-// const moreBtn = document.querySelector('#showmore');
-// const reviews = document.getElementsByClassName('.customerReviews');
-
-// moreBtn.addEventListener('click', () => {
-
-//   reviews.classList.add("textBold");
-// })
-
-//  ATTEMPT #3
-
-// Hide all reviews after the first 5 reviews
-
+// Hide all reviews after the 5th review
 document.querySelectorAll('.customerReviews:nth-of-type(n+6)').forEach(item => {
   item.style.display = 'none';
-
-  // moreBtn.addEventListener('click', () => {
-  //   const reviews = document.querySelectorAll('.customerReviews');
-  
-  //   for (let i = currentIndex; i <= reviews.length; i += 5) {
-  //     if (reviews) {
-  //       reviews.classList.add('showReview');      
-  //     }
-  //   }
-  
-  
-    // currentIndex += 5;
-    
-    // If you display all the elements then hide the show more button
-  //   if (currentIndex >= reviews.length) {
-  //     moreBtn.style.display = 'none';
-  //   }
-  // })
 })
