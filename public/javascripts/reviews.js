139cc01b76b43dd19ea1ff86d51ce041e3764fb1
lufefe98 <lufefezyn@gmail.com>
2026-04-16 20:03:08 +0200

    Add change in review button dropdown button and menu

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -1,7 +1,11 @@
-// // Hide/Reveal Review Form
+// Hide/Reveal Review Form
 
 const reviewBtn = document.querySelector(".review-btn")
 const form = document.getElementById("reviewsForm")
+
+const reviewsOptionBtn = document.querySelectorAll(".review-options-btn")
+const reviewsOptionDropdown = document.querySelectorAll(".review-options-dropdown")
+const editFormSection = document.getElementById("edit-reviews-container")
 
 
 form.style.display = 'none'
@@ -17,3 +21,29 @@ function toggleformDisplay() {
 }
 
 reviewBtn.addEventListener('click', toggleformDisplay)
+
+
+// Hide/Display Edit/ Options
+
+// function toggleReviewOptions() {
+//   reviewsOptionDropdown[0].showPicker()
+//       // reviewsOptionDropdown[0].style.display = 'none'
+//       console.log('triggered')
+//   // if (editFormSection.style.display = 'none') {
+//     //   editFormSection.style.display = 'inline-block'
+//     // } else {
+//       //   editFormSection.style.display = 'none'
+//       // }
+//     }
+    reviewsOptionDropdown[0].style.display = 'inline-block'
+    
+reviewsOptionBtn[0].addEventListener('click', function () {
+  reviewsOptionDropdown[0].showPicker()
+      console.log('triggered')
+    })
+
+
+
+// Hide/Display Edit Review Form
+
+
