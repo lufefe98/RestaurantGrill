a7bf94ce483803d7694637f7ceb88bd288e8dbcf
lufefe98 <lufefezyn@gmail.com>
2026-04-23 22:11:39 +0200

    Add toggle review card dropdown menu toggle

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -6,6 +6,7 @@ const form = document.getElementById("reviewsForm")
 const reviewsOptionBtn = document.querySelectorAll(".review-options-btn")
 const reviewsOptionDropdown = document.querySelectorAll(".review-options-dropdown")
 const editFormSection = document.getElementById("edit-reviews-container")
+const reviewCard = document.querySelectorAll(".review-card")
 
 
 form.style.display = 'none'
@@ -23,27 +24,57 @@ function toggleformDisplay() {
 reviewBtn.addEventListener('click', toggleformDisplay)
 
 
-// Hide/Display Edit/ Options
+// Hide/Display Edit/Delete Options
 
-// function toggleReviewOptions() {
-//   reviewsOptionDropdown[0].showPicker()
-//       // reviewsOptionDropdown[0].style.display = 'none'
-//       console.log('triggered')
-//   // if (editFormSection.style.display = 'none') {
-//     //   editFormSection.style.display = 'inline-block'
-//     // } else {
-//       //   editFormSection.style.display = 'none'
-//       // }
-//     }
-    reviewsOptionDropdown[0].style.display = 'inline-block'
-    
-reviewsOptionBtn[0].addEventListener('click', function () {
-  reviewsOptionDropdown[0].showPicker()
-      console.log('triggered')
-    })
+// Loop through the review cards, get click feature on all
+// buttons, get it to open the options for that review card
+// and make those buttons clickable too (ie: edit & delete)
+
+reviewCard.forEach(card => {
+  // Inputs: card, button, list of (hidden) dropdown options
+  const reviewOptionsBtn = card.querySelector('.review-options-btn')
+  const reviewDropdownItem = card.querySelectorAll('.review-dropdown-item')
+
+
+  // Outputs: Visible dropdown options
+  reviewOptionsBtn.addEventListener('click', () => {
+    for (let i = 0; i < reviewDropdownItem.length; i++) {
+      if (reviewDropdownItem[i].style.display === '') {
+        reviewDropdownItem[i].style.display = 'flex'
+        console.log(`${reviewDropdownItem[i].style.display}`)
+      } else if (reviewDropdownItem[i].style.display === 'flex'){
+        reviewDropdownItem[i].style.display = 'none'
+        console.log(`${reviewDropdownItem[i].style.display}`)
+      } else {
+        reviewDropdownItem[i].style.display = 'flex'
+        console.log(`${reviewDropdownItem[i].style.display}`)
+      }
+    }
+  })
+  // Edge cases: All the other dropdown menus could be
+  // activated, the dropdown menu has to go away if I click
+  // anywhere else, only one dropdown can be active at a time.
+
+
+});
+
+
+
 
 
 
 // Hide/Display Edit Review Form
 
 
+
+
+// Ignore this:
+
+// const nkinobho = document.querySelectorAll(".review-options-btn")
+// nkinobho[0].addEventListener('click', () => {
+//     const reviewOptions = document.querySelectorAll('.review-dropdown-item')
+
+//     for (let i = 0; i < reviewOptions.length; i++) {
+//      reviewOptions[i].style.display = 'flex'
+//     }
+// })
